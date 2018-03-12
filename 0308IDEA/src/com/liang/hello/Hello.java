b46c05472578237bf78f203de744da13a01769d7
aiwalle <wallelj0909@gmail.com>
2018-03-12 16:10:47 +0800

    add

---
+++ 0308IDEA/src/com/liang/hello/Hello.java
@@ -0,0 +1,18 @@
+package com.liang.hello;
+
+/**
+ * Created by liang on 2018/3/8.
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+
+    public void  hello() {
+
+    }
+
+
+}
---
+++ 0308IDEA/src/com/liang/hello/Other.java
@@ -0,0 +1,14 @@
+package com.liang.hello;
+
+import lombok.Data;
+
+/**
+ * Created by liang on 2018/3/8.
+ */
+
+@Data
+public class Other {
+
+    private String name;
+    private Integer age;
+}
